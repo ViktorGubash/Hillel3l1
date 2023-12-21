79552f252d603fa1c2cd1dfcd2bd1416a449aef4
Viktor Gubash <viktor.gubash@school140.org>
2023-12-21 23:11:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log('Sup', 'howdy?')
+
+// alert("AAAAAAA")
+
+// let myName1 = 'Viktor'
+// let myAge = '15'
+// console.log('My name is', myName1, 'I am ', myAge, 'years old')
+
+let userName
+userName = prompt('Please, input your name')
+console.log(userName)
+alert('Hello, ' + userName + '!' + ' How are you doing?')
